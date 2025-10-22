420c89e8a90e0b2ab836080a231541980afbe93b
KauaMelchioretto <kauamelchoretto@gmail.com>
2025-10-22 19:43:30 -0300

    feature(): adjusts internationalizations terms to kanban and roles modules and adjust some exception messages

--- src/main/kotlin/com/delice/crm/core/user/domain/exceptions/UserException.kt
+++ src/main/kotlin/com/delice/crm/core/user/domain/exceptions/UserException.kt
@@ -5,8 +5,8 @@ import com.delice.crm.core.utils.exception.DefaultError
 val USER_NOT_FOUND = UserException("USER_NOT_FOUND", "User not found")
 val USER_UNEXPECTED = UserException("USER_UNEXPECTED", "An unexpected error occurred")
 
-val NAME_MUST_BE_PROVIDED = UserException("NAME_MUST_BE_PROVIDED", "Name or surname must provided")
-val EMAIL_MUST_BE_PROVIDED = UserException("EMAIL_MUST_BE_PROVIDED", "Email must provided")
+val NAME_MUST_BE_PROVIDED = UserException("NAME_MUST_BE_PROVIDED", "Name or surname must be provided")
+val EMAIL_MUST_BE_PROVIDED = UserException("EMAIL_MUST_BE_PROVIDED", "Email must be provided")
 val CITY_MUST_BE_PROVIDED = UserException("CITY_MUST_BE_PROVIDED", "The city of user must be provided")
 val STATE_MUST_BE_PROVIDED = UserException("STATE_MUST_BE_PROVIDED", "The state of user must be provided")
 val ZIP_CODE_MUST_BE_PROVIDED = UserException("ZIP_CODE_MUST_BE_PROVIDED", "The zip code of user must be provided")
